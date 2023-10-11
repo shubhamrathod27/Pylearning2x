21ad1d85d0b13a95e646a45964530c42ed6fde45
shubham rathod <83837868+shubhamrathod27@users.noreply.github.com>
2023-10-11 03:24:09 -0400

    Create Lab013.py

---
+++ 6_OCT_2023/Lab013.py
@@ -0,0 +1,12 @@
+my_string = "This is Shubham From The Testing Academy"
+print(type(my_string))
+print(len(my_string))
+
+# String is bunch of char
+# shubham -> s h u b h a m
+name = "shubham"
+print(len(name))
+
+# lengh - java / pyh - never starts from 0
+# index - 0 to len-1
+
